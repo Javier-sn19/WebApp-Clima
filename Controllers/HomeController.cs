b5d141b50cdcfec211c573e7b8eda40e99c08c58
Javier-sn19 <132592872+Javier-sn19@users.noreply.github.com>
2024-02-24 02:42:08 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using API.Models;
+
+namespace API.Controllers
+{
+    public class HomeController : Controller
+    {
+       [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new WeatherHelper());
+        }
+        [HttpPost]
+        public ActionResult GetWeatherForecast(string InputCity)
+        {
+            WeatherHelper weather = new WeatherHelper();
+            weather.InputCity = InputCity;
+            return Json(weather.GetWeatherList(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
